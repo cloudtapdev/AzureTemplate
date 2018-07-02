c79557c39ff882653f0872b65fee9cfe02f70ade
Popa Maria-Catalina <popa.catalina01@gmail.com>
2018-07-02 15:32:05 +0300

    add newline

--- deployAgent.sh
+++ deployAgent.sh
@@ -6,3 +6,5 @@ sudo service docker start
 
 # $1 project key
 docker run -v /:/host -v /var/run/docker.sock:/var/run/docker.sock --privileged --name cloudlensCollectorAgent -d --restart=on-failure --net=host ixiacom/cloudlens-agent --accept_eula yes --apikey $1 --server catadev-agent.ixia-cloudlens.net
+
+echo "success"
