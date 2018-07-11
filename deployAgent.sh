3023e7f2a465faa71b229b43d444b7a3c5a6cd60
cloudtapdev <37666717+cloudtapdev@users.noreply.github.com>
2018-07-11 10:25:07 +0300

    Update deployAgent.sh

--- deployAgent.sh
+++ deployAgent.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y docker.io
 sudo service docker start
 
 # $1 project key
-docker run -v /:/host -v /var/run/docker.sock:/var/run/docker.sock --privileged --name cloudlensCollectorAgent -d --restart=on-failure --net=host ixiacom/cloudlens-agent --accept_eula yes --apikey $1 --server catadev-agent.ixia-cloudlens.net
+docker run -v /:/host -v /var/run/docker.sock:/var/run/docker.sock --privileged --name cloudlensCollectorAgent -d --restart=on-failure --net=host ixiacom/cloudlens-agent --accept_eula yes --apikey $1 --server $2
 
 echo "success"
